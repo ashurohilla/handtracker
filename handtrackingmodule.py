40c520816a957b39cc5303bb6c082fb36b0249fc
Rishabh Varshney <right.rishabh@gmail.com>
2022-11-10 13:44:01 +0530

    volume and numbers

--- handtrackingmodule.py
+++ handtrackingmodule.py
@@ -32,10 +32,10 @@ class handDetector():
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
             for id, lm in enumerate(myHand.landmark):
-                print(id, lm)
+                # print(id, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                print(id, cx, cy)
+                # print(id, cx, cy)
                 lmList.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
@@ -51,8 +51,8 @@ def main():
         success, img = cap.read()
         img = detector.findHands(img)
         lmList = detector.findPosition(img)
-        if len(lmList) != 0:
-            print(lmList[4])
+        # if len(lmList) != 0:
+        #     print(lmList[4])
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
